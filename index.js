8f3777522fdda6008da1a3d53ace1e1bbe08c9f5
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-03-27 14:33:41 -0400

    finishes lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(record) {
+    var win = record.find(function(element) {
+      return element.result === "W";
+    });
+  
+    return win ? win.year : undefined;
+  }
